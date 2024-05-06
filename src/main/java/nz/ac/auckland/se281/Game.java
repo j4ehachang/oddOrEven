ebfca1f8678122f6bac9e24ca29d56362d308f2b
Jaeha <jcha513@aucklanduni.ac.nz>
2024-05-06 17:47:07 +1200

    Passed second test case for showing the current round

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,11 +6,18 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  private static int round;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    round = 0;
+
   }
 
-  public void play() {}
+  public void play() {
+    round ++;
+    MessageCli.START_ROUND.printMessage(Integer.toString(round));
+  }
 
   public void endGame() {}
 
