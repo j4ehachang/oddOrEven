324127fd89a7b02922e0ce76361a1f5339c52192
Jaeha <jcha513@aucklanduni.ac.nz>
2024-05-09 15:44:24 +1200

    Store the last used strategy in a varaible to use later

--- src/main/java/nz/ac/auckland/se281/Hard.java
+++ src/main/java/nz/ac/auckland/se281/Hard.java
@@ -4,11 +4,13 @@ import java.util.List;
 import nz.ac.auckland.se281.Main.Choice;
 
 public class Hard implements DifficultyLevel {
+ private Strategy lastStrategy;
 
   @Override
   public int selectFingers(List<Integer> roundList, Choice choice, Boolean aiWonLastRound) {
     if (roundList.size() < 3) {
       ChooseStrategy choosenStrategy = new ChooseStrategy(new Random(), roundList, choice);
+      lastStrategy = choosenStrategy.getStrategy();
       return choosenStrategy.process();
 
     } else {
