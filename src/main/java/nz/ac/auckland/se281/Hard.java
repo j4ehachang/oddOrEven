afa54f05aaab94fd3a7a5d501172dedef8326703
Jaeha <jcha513@aucklanduni.ac.nz>
2024-05-09 16:00:07 +1200

    The ai correctly implements the hard difficulty to play the game

--- src/main/java/nz/ac/auckland/se281/Hard.java
+++ src/main/java/nz/ac/auckland/se281/Hard.java
@@ -4,19 +4,32 @@ import java.util.List;
 import nz.ac.auckland.se281.Main.Choice;
 
 public class Hard implements DifficultyLevel {
- private Strategy lastStrategy;
+  private Strategy lastStrategy;
 
   @Override
   public int selectFingers(List<Integer> roundList, Choice choice, Boolean aiWonLastRound) {
+
     if (roundList.size() < 3) {
       ChooseStrategy choosenStrategy = new ChooseStrategy(new Random(), roundList, choice);
       lastStrategy = choosenStrategy.getStrategy();
       return choosenStrategy.process();
 
-    } else {
-
-      ChooseStrategy choosenStrategy = new ChooseStrategy(new Top(), roundList, choice);
+    } else if (aiWonLastRound) {
+      ChooseStrategy choosenStrategy = new ChooseStrategy(lastStrategy, roundList, choice);
+      lastStrategy = choosenStrategy.getStrategy();
       return choosenStrategy.process();
+
+    } else {
+      if (lastStrategy instanceof Random) {
+        ChooseStrategy choosenStrategy = new ChooseStrategy(new Top(), roundList, choice);
+        lastStrategy = choosenStrategy.getStrategy();
+        return choosenStrategy.process();
+
+      } else {
+        ChooseStrategy choosenStrategy = new ChooseStrategy(new Random(), roundList, choice);
+        lastStrategy = choosenStrategy.getStrategy();
+        return choosenStrategy.process();
+      }
     }
   }
 }
